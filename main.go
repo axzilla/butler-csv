8c6a1f45aa921b274c2a2e35ad1e86dee2fffdd4
DerbeDotDev <moin@derbe.dev>
2023-08-21 12:44:49 +0200

    Read first csv file

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world")
+	transactions, err := os.Open("transactions.csv")
+	if err != nil {
+		log.Fatalf("Error on opening file: %v", err)
+	}
+
+	reader := csv.NewReader(transactions)
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatalf("Error on reading file: %v", err)
+	}
+
+	for _, record := range records {
+		fmt.Println(record)
+	}
 }
